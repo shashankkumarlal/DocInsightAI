215438e97ea6ea032feb14ab15e90cb75bab1958
SHASHANK KUMAR LAL <87319766+shashankkumarlal@users.noreply.github.com>
2025-03-20 14:00:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,4 +76,4 @@ def developer():
     return render_template('developer.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
